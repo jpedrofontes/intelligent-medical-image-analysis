30d065650957deff6e8657a4102d01cfabc95ba9
João Pedro Fontes <jpedrofontes8@gmail.com>
2018-02-12 16:19:54 +0000

    Switch to keras API

--- Project/bcdr.py
+++ Project/bcdr.py
@@ -2,10 +2,6 @@ import os
 import csv
 import cv2 as cv
 import numpy as np
-import tensorflow as tf
-
-from tensorflow.python.framework import dtypes
-from tensorflow.python.framework.ops import convert_to_tensor
 
 class Reader(object):
     '''
@@ -13,7 +9,7 @@ class Reader(object):
     Requires Tensorflow >= version 1.12rc0
     '''
 
-    def __init__(self, path, csv, mode, batch_size, num_classes, shuffle=True, buffer_size=1000, save=False):
+    def __init__(self, path, csv, num_classes, shuffle=True, save=False):
         '''
         Create a new BCDR Tensorflow instance.
         Recieves a path string to a csv file, which consists of many lines,
@@ -38,147 +34,59 @@ class Reader(object):
         self.csv = csv
         self.save = save
         self.num_classes = num_classes
-        # Retrieve the data from the text file
+        # Retrieve the data from the csv file
         self._read_csv_file()
-        # Number of samples in the dataset
-        self.data_size = len(self.labels)
-        # Initial shuffling of the file and label lists (together!)
-        if shuffle:
-            self._shuffle_lists()
-        # Convert lists to TF tensor
-        self.img_paths = convert_to_tensor(self.img_paths, dtype=dtypes.string)
-        self.labels = convert_to_tensor(self.labels, dtype=dtypes.int32)
-        self.min_x = convert_to_tensor(self.min_x, dtype=dtypes.int32)
-        self.min_y = convert_to_tensor(self.min_y, dtype=dtypes.int32)
-        self.max_x = convert_to_tensor(self.max_x, dtype=dtypes.int32)
-        self.max_y = convert_to_tensor(self.max_y, dtype=dtypes.int32)
-        # Create dataset
-        data = tf.data.Dataset.from_tensor_slices((self.img_paths, self.labels,
-                                                   self.min_x, self.min_y,
-                                                   self.max_x, self.max_y))
-        # Distinguish between train/infer. when calling the parsing functions
-        if mode == 'training':
-            data = data.map(self._parse_function_train)
-        elif mode == 'inference':
-            data = data.map(self._parse_function_inference)
-        else:
-            raise ValueError("Invalid mode '%s'." % (mode))
-        # Shuffle the first `buffer_size` elements of the dataset
-        if shuffle:
-            data = data.shuffle(buffer_size=buffer_size)
-        # Create a new dataset with batches of images
-        data = data.batch(batch_size)
-        self.data = data
 
     def _read_csv_file(self):
         '''
         Read the content of the csv file and store it into lists.
         '''
-        self.img_paths = []
-        self.labels = []
-        self.min_x = []
-        self.min_y = []
-        self.max_x = []
-        self.max_y = []
+        self.data = np.array([])
+        self.labels = np.array([])
         with open(os.path.join(self.path, self.csv), 'r') as raw_data:
             outlines_reader = csv.DictReader(raw_data, delimiter=',')
             for row in outlines_reader:
-                path = row['image_filename'][1:]
-                self.img_paths.append(os.path.join(self.path, path))
-                self.labels.append(int(row['classification'][1:] == 'Malign'))
-                if row['classification'][1:] == 'Malign':
-                    color = (255,0,0)
-                elif row['classification'][1:] == 'Benign':
-                    color = (0,255,0)
+                path = os.path.join(self.path, row['image_filename'][1:])
+                img = cv.imread(path)
+                # Benign => green
+                # Malign => red
+                if row['classification'][1:] == 'Benign':
+                    color = (0, 255, 0)
+                    self.labels = np.append(self.labels, [0])
+                elif row['classification'][1:] == 'Malign':
+                    color = (255, 0, 0)
+                    self.labels = np.append(self.labels, [1])
                 else:
-                    print('Error in patient with id {}, study {}. Continuing...'.format(row['patient_id'], row['study_id']))
+                    print('Error on study {} from patient with id {}, ignoring...'.format(row['study_id'], row['patient_id']))
                     continue
                 # Get lesion bounding points
                 x_points = np.fromstring(row['lw_x_points'], sep=' ')
                 y_points = np.fromstring(row['lw_y_points'], sep=' ')
-                # Save with bouding points?
-                if self.save:
-                    img = cv.imread(os.path.join(self.path, path))
+                if self.save == True:
                     for i in range (0, x_points.size-2):
                         cv.line(img, (int(x_points[i]), int(y_points[i])), (int(x_points[i+1]), int(y_points[i+1])), color, 3)
                     cv.line(img, (int(x_points[x_points.size-1]), int(y_points[x_points.size-1])), (int(x_points[0]), int(y_points[0])), color, 3)
-                    cv.imwrite(os.path.join(self.path, '/bounded/', row['image_filename'][1:]), img)
+                    cv.imwrite('lines/' + row['image_filename'][1:], img)
                 # Get bounding box [y,x]
-                self.min_x.append(int(min(x_points)-10))
-                self.min_y.append(int(min(y_points)-10))
-                self.max_x.append(int(max(x_points)+10))
-                self.max_y.append(int(max(y_points)+10))
+                min_x = int(min(x_points)-10)
+                min_y = int(min(y_points)-10)
+                max_x = int(max(x_points)+10)
+                max_y = int(max(y_points)+10)
+                # cv.rectangle(img, (min_x, min_y), (max_x, max_y), color, 5)
+                roi_img = img[min_y:max_y, min_x:max_x]
+                try:
+                    cv.resize(roi_img, (32, 32))
+                    if self.save == True:
+                        cv.imwrite('cropped/' + row['image_filename'][1:], roi_img)
+                    self.data = np.append(self.data, [roi_img])
+                except:
+                    pass
 
-    def _shuffle_lists(self):
-        '''
-        Conjoined shuffling of the list of paths and labels.
-        '''
-        path = self.img_paths
-        labels = self.labels
-        permutation = np.random.permutation(self.data_size)
-        self.img_paths = []
-        self.labels = []
-        for i in permutation:
-            self.img_paths.append(path[i])
-            self.labels.append(labels[i])
-
-    def _parse_function_train(self, filename, label, min_x, min_y, max_x, max_y):
-        '''
-        Input parser for samples of the training set.
-        '''
-        # Convert label number into one-hot-encoding
-        one_hot = tf.one_hot(label, self.num_classes)
-        # Load and preprocess the image
-        img_string = tf.read_file(filename)
-        img_decoded = tf.image.decode_png(img_string, channels=3)
-        img_resized = tf.image.resize_images(img_decoded, [227, 227])
-        # img = cv.imread(os.path.join(self.path, filename))
-        # Crop ROI
-        # img = img[min_y:max_y, min_x:max_x]
-        # ... global contrast norm
-        # ... local contrast norm
-        # ... resize
-        return img_resized, one_hot
-
-    def _parse_function_inference(self, filename, label, min_x, min_y, max_x, max_y):
-        '''
-        Input parser for samples of the validation/test set.
-        '''
-        # Convert label number into one-hot-encoding
-        one_hot = tf.one_hot(label, self.num_classes)
-        # Load and preprocess the image
-        img_string = tf.read_file(filename)
-        img_decoded = tf.image.decode_png(img_string, channels=3)
-        img_resized = tf.image.resize_images(img_decoded, [227, 227])
-        # img = cv.imread(os.path.join(self.path, filename))
-        # Crop ROI
-        # img = img[min_y:max_y, min_x:max_x]
-        # ... global contrast norm
-        # ... local contrast norm
-        # ... resize
-        return img_resized, one_hot
-
-    def _global_contrast_normalization(img, s, lmda, epsilon):
-        '''
-        Global constrast normalization image filter.
-        '''
-        # Get mean value from the image
-        img_average = np.mean(img)
-        # Apply filter
-        img = img - img_average
-        contrast = np.sqrt(lmda + np.mean(img**2))
-        img = (s * img) / max(contrast, epsilon)
-        return img
-
-    def _local_contrast_normalization(img):
-        '''
-        Local contrast normalization image filter.
-        '''
-        pass
-
-    def _data_augmentation(img):
-        '''
-        Data augmentation process.
-        Returns 4 versions of the input image, rotated in 90, 180 and 270 degrees.
-        '''
-        pass
+    def load_data(self):
+        size = len(self.data)
+        print(size)
+        x_train = self.data[0:int(0.66*size)]
+        y_train = self.labels[0:int(0.66*size)]
+        x_test = self.data[int(0.66*size):size]
+        y_test = self.labels[int(0.66*size):size]
+        return (x_train, y_train), (x_test, y_test)
--- Project/breast_cancer_diagnosis.py
+++ Project/breast_cancer_diagnosis.py
@@ -2,25 +2,21 @@
 import warnings
 warnings.simplefilter("ignore")
 
-# Suppress tensorflow logging information
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
 # Imports
+import os
 import sys
+import keras
 import cv2 as cv
 import numpy as np
-import tensorflow as tf
 import matplotlib.pyplot as plt
 
-from datetime import datetime
+from classifiers import CNN
 from bcdr import Reader as bcdr_reader
-from classifiers import AlexNet as alex_net
-from tensorflow.contrib.data import Iterator
+from keras.preprocessing.image import ImageDataGenerator
 
 # OpenCV an Tensorflow versions
 print("OpenCV Version: {}".format(cv.__version__))
-print("Tensorflow Version: {}".format(tf.__version__))
+print("Keras Version: {}\n".format(keras.__version__))
 
 # Check BCDR instance to use
 current_path = os.getcwd()
@@ -38,144 +34,78 @@ else:
             print('\t- {}'.format(dir))
         sys.exit()
 
-# Learning params
-learning_rate = 0.00001
-num_epochs = 10
-batch_size = 16
-
-# Network params
-dropout_rate = 0.5
+batch_size = 32
 num_classes = 2
-train_layers = ['fc8', 'fc7', 'fc6']
+epochs = 100
+num_predictions = 20
 
-# How often we want to write the tf.summary data to disk
-display_step = 20
+# The data, shuffled and split between train and test sets:
+(x_train, y_train), (x_test, y_test) = bcdr_reader(dataset_path,
+                                                   'outlines.csv',
+                                                   num_classes).load_data()
 
-# Path for tf.summary.FileWriter and to store model checkpoints
-filewriter_path = "/Users/joaopedrofontes/Documents/UM/5 Ano/Dissertacao/Dissertation/Project/alexnet/summary"
-checkpoint_path = "/Users/joaopedrofontes/Documents/UM/5 Ano/Dissertacao/Dissertation/Project/alexnet/checkpoint"
+# Convert class vectors to binary class matrices.
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
 
-# Create parent path if it doesn't exist
-if not os.path.isdir(checkpoint_path):
-    os.mkdir(checkpoint_path)
+model2 = CNN(num_classes, x_train[0].shape).createModel()
 
-# Place data loading and preprocessing on the cpu
-with tf.device('/cpu:0'):
-    tr_data = bcdr_reader(dataset_path,
-                          'outlines_train.csv',
-                          mode='training',
-                          batch_size=batch_size,
-                          num_classes=num_classes,
-                          save=True)
-    val_data = bcdr_reader(dataset_path,
-                           'outlines_test.csv',
-                           mode='inference',
-                           batch_size=batch_size,
-                           num_classes=num_classes,
-                           shuffle=False)
-    # create an reinitializable iterator given the dataset structure
-    iterator = Iterator.from_structure(tr_data.data.output_types,
-                                       tr_data.data.output_shapes)
-    next_batch = iterator.get_next()
+# initiate RMSprop optimizer
+opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
 
-# Ops for initializing the two different iterators
-training_init_op = iterator.make_initializer(tr_data.data)
-validation_init_op = iterator.make_initializer(val_data.data)
-# TF placeholder for graph input and output
-x = tf.placeholder(tf.float32, [batch_size, 227, 227, 3])
-y = tf.placeholder(tf.float32, [batch_size, num_classes])
-keep_prob = tf.placeholder(tf.float32)
+# Let's train the model using RMSprop
+model.compile(loss='categorical_crossentropy',
+              optimizer=opt,
+              metrics=['accuracy','loss'])
 
-# Initialize model
-model = alex_net(x, keep_prob, num_classes, train_layers, '/Users/joaopedrofontes/Documents/UM/5 Ano/Dissertacao/Dissertation/Project/alexnet/bvlc_alexnet.npy')
-# Link variable to model output
-score = model.fc8
-# List of trainable variables of the layers we want to train
-var_list = [v for v in tf.trainable_variables() if v.name.split('/')[0] in train_layers]
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
 
-# Op for calculating the loss
-with tf.name_scope("cross_ent"):
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=score, labels=y))
+# This will do preprocessing and realtime data augmentation:
+datagen = ImageDataGenerator(
+    featurewise_center=False,  # set input mean to 0 over the dataset
+    samplewise_center=False,  # set each sample mean to 0
+    featurewise_std_normalization=False,  # divide inputs by std of the dataset
+    samplewise_std_normalization=False,  # divide each input by its std
+    zca_whitening=False,  # apply ZCA whitening
+    rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+    width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+    height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+    horizontal_flip=True,  # randomly flip images
+    vertical_flip=False)  # randomly flip images
 
-# Train op
-with tf.name_scope("train"):
-    # Get gradients of all trainable variables
-    gradients = tf.gradients(loss, var_list)
-    gradients = list(zip(gradients, var_list))
-    # Create optimizer and apply gradient descent to the trainable variables
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    train_op = optimizer.apply_gradients(grads_and_vars=gradients)
+# Compute quantities required for feature-wise normalization
+# (std, mean, and principal components if ZCA whitening is applied).
+datagen.fit(x_train)
 
-# Add gradients to summary
-for gradient, var in gradients:
-    tf.summary.histogram(var.name + '/gradient', gradient)
+# Fit the model on the batches generated by datagen.flow().
+history = model.fit_generator(datagen.flow(x_train, y_train,
+                                 batch_size=batch_size),
+                              epochs=epochs,
+                              validation_data=(x_test, y_test),
+                              workers=4)
 
-# Add the variables we train to the summary
-for var in var_list:
-    tf.summary.histogram(var.name, var)
+# Score trained model.
+scores = model.evaluate(x_test, y_test, verbose=1)
+print('Test loss:', scores[0])
+print('Test accuracy:', scores[1])
 
-# Add the loss to summary
-tf.summary.scalar('cross_entropy', loss)
+# Loss Curves
+plt.figure(figsize=[8,6])
+plt.plot(history.history['loss'],'r',linewidth=3.0)
+plt.plot(history.history['val_loss'],'b',linewidth=3.0)
+plt.legend(['Training loss', 'Validation Loss'],fontsize=18)
+plt.xlabel('Epochs ',fontsize=16)
+plt.ylabel('Loss',fontsize=16)
+plt.title('Loss Curves',fontsize=16)
 
-# Evaluation op: Accuracy of the model
-with tf.name_scope("accuracy"):
-    correct_pred = tf.equal(tf.argmax(score, 1), tf.argmax(y, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-
-# Add the accuracy to the summary
-tf.summary.scalar('accuracy', accuracy)
-# Merge all summaries together
-merged_summary = tf.summary.merge_all()
-# Initialize the FileWriter
-writer = tf.summary.FileWriter(filewriter_path)
-# Initialize a saver for store model checkpoints
-saver = tf.train.Saver()
-# Get the number of training/validation steps per epoch
-train_batches_per_epoch = int(np.floor(tr_data.data_size / batch_size))
-val_batches_per_epoch = int(np.floor(val_data.data_size / batch_size))
-
-# Start Tensorflow session
-with tf.Session() as sess:
-    # Initialize all variables
-    sess.run(tf.global_variables_initializer())
-    # Add the model graph to TensorBoard
-    writer.add_graph(sess.graph)
-    # Load the pretrained weights into the non-trainable layer
-    model.load_initial_weights(sess)
-
-    print("{} Start training...".format(datetime.now()))
-    print("{} Open Tensorboard at --logdir {}".format(datetime.now(), filewriter_path))
-
-    # Loop over number of epochs
-    for epoch in range(num_epochs):
-        print("{} Epoch number: {}".format(datetime.now(), epoch+1))
-        # Initialize iterator with the training dataset
-        sess.run(training_init_op)
-        for step in range(train_batches_per_epoch):
-            # get next batch of data
-            img_batch, label_batch = sess.run(next_batch)
-            # And run the training op
-            sess.run(train_op, feed_dict={ x: img_batch, y: label_batch, keep_prob: dropout_rate })
-            # Generate summary with the current batch of data and write to file
-            if step % display_step == 0:
-                s = sess.run(merged_summary, feed_dict={ x: img_batch, y: label_batch, keep_prob: 1. })
-                writer.add_summary(s, epoch * train_batches_per_epoch + step)
-
-        # Validate the model on the entire validation set
-        print("{} Start validation".format(datetime.now()))
-        sess.run(validation_init_op)
-        test_acc = 0.
-        test_count = 0
-        for _ in range(val_batches_per_epoch):
-            img_batch, label_batch = sess.run(next_batch)
-            acc = sess.run(accuracy, feed_dict={ x: img_batch, y: label_batch, keep_prob: 1. })
-            test_acc += acc
-            test_count += 1
-        test_acc /= test_count
-        print("{} Validation Accuracy = {:.4f}".format(datetime.now(), test_acc))
-        print("{} Saving checkpoint of model...".format(datetime.now()))
-
-        # Save checkpoint of the model
-        checkpoint_name = os.path.join(checkpoint_path, 'model_epoch'+str(epoch+1)+'.ckpt')
-        save_path = saver.save(sess, checkpoint_name)
-        print("{} Model checkpoint saved at {}".format(datetime.now(), checkpoint_name))
+# Accuracy Curves
+plt.figure(figsize=[8,6])
+plt.plot(history.history['acc'],'r',linewidth=3.0)
+plt.plot(history.history['val_acc'],'b',linewidth=3.0)
+plt.legend(['Training Accuracy', 'Validation Accuracy'],fontsize=18)
+plt.xlabel('Epochs ',fontsize=16)
+plt.ylabel('Accuracy',fontsize=16)
+plt.title('Accuracy Curves',fontsize=16)
